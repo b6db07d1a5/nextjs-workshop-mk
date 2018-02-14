f666a0abb06b947f34131e78d542991ad71947ae
Sitthichai Wanthong <sitthichaiw@Ananass-MacBook-Pro.local>
2018-02-14 15:45:23 +0700

    redesign finish cart item

--- components/ContentCart.js
+++ components/ContentCart.js
@@ -36,23 +36,62 @@ class ContentCart extends Component {
     render() {
         const { menus, sum } = this.state
         return (
-            <div>
-                <div>
-                    {menus.map((menu, i) => 
-                        <div key={`menu cart ${i}`}>
-                        {menu.name} 
-                        <button onClick={this.props.onClickDecreaseFromCart(menu.id)}> - </button>
-                        {menu.qty}
-                        <button onClick={this.props.onClickIncreaseFromCart(menu.id)}> + </button>
-                        </div>)}
+                <div style={styles}>
+                    <div style={styles.divList}>
+                        {menus.map((menu, i) => 
+                            <div key={`menu cart ${i}`} style={styles.item}>
+                                <p>{i+1}. </p>
+                                <img style={styles.item.image} src={`static/images/menus/${menu.images}`} />
+                                <p style={styles.item.name}>{menu.name}</p>
+                                <p> {menu.qty} ea. </p>
+                                <div style={styles.item.divBtn}>
+                                    <button style={styles.item.button} onClick={this.props.onClickIncreaseFromCart(menu.id)}> + </button>
+                                    <button style={styles.item.button} onClick={this.props.onClickDecreaseFromCart(menu.id)}> - </button>
+                                </div>
+                            </div>)}
+                        
+                    </div>
+                    <div style={{padding: 20}}>
+                        <span > Total Price : {sum} </span>
+                    </div>
                 </div>
-                =-=-=-=-=-=-=-=-=-=-=-= <br />
-                {sum}
-            </div>
         );
     }
 }
 
+const styles = {
+    display: 'flex',
+    divList: {
+        height: '100vh',
+        overflow: 'auto'
+    },
+    item : {
+        display: 'flex',
+        padding: 20,
+        image: {
+            width: 50,
+            height: 50
+        },
+        name: {
+            width: 150
+        },
+        divBtn: {
+            paddingLeft: 20,
+            height: 60,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        },
+        button: {
+            backgroundColor: 'rgba(244, 133, 7, 0.8)',
+            border: 'none',
+            color: 'white',
+            padding: 5,
+            textAlign: 'center',
+            height: 25
+        }
+    }
+}
 
 
 export default ContentCart;
